9182d1fe1e224212374d0382d8a026d9ff87ae8a
Ifeanyi Ayodeji Ukomadu <ifeanyiapple1@gmail.com>
2025-07-31 22:58:31 +0100

    Remove MongoDB initialization from browser

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { initializeServiceProviders } from "./lib/mongodb";
-
-// Initialize database collections
-initializeServiceProviders().catch(console.error);
 
 createRoot(document.getElementById("root")!).render(<App />);
